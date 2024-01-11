2e0ba8c692ca1ac5386028f6ab345042e2eb05d8
pawan706b <pawankumark54382@gmail.com>
2024-01-11 15:56:40 -0400

    update transform

--- src/transform.py
+++ src/transform.py
@@ -1,7 +1,17 @@
 # src/transform.py
+import numpy as np
+
 def transform(df):
     ''' This function converts the GDP information from Currency
     format to float value, transforms the information of GDP from
     USD (Millions) to USD (Billions) rounding to 2 decimal places.
     The function returns the transformed dataframe.'''
+
+    data_list = df['GDP_USD_millions'].tolist()
+    data_list = [float("".join(x.split(','))) for x in data_list]
+    data_list = [np.round(x/1000,2) for x in data_list]
+    df['GDP_USD_millions'] = data_list
+    df = df.rename(columns={'GDP_USD_millions':'GDP_USD_billions'})
+    print(df['GDP_USD_billions'])
     return df
+
