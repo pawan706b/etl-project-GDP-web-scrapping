a7acc0145a10ad27010a07a12dc7f66b59735ba2
pawan706b <pawankumark54382@gmail.com>
2024-01-11 16:12:49 -0400

    update query

--- src/query.py
+++ src/query.py
@@ -1,4 +1,8 @@
 # src/query.py
+import pandas as pd
+
 def run_query(query_statement, sql_connection):
     ''' This function runs the stated query on the database table and
     prints the output on the terminal. Function returns nothing. '''
+    output = pd.read_sql(query_statement, sql_connection)
+    print(output)
