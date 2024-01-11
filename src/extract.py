e63b2643c7670dbc4a65f1ebb6187fe44596c78f
pawan706b <pawankumark54382@gmail.com>
2024-01-11 15:26:20 -0400

    extract update

--- src/extract.py
+++ src/extract.py
@@ -4,6 +4,8 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from transform import transform
 
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
+table_attributes = ["Country", "GDP_USD_millions"]
 def extract(url, table_attribs):
     ''' This function extracts the required
     information from the website and saves it to a dataframe. The
@@ -15,14 +17,16 @@ def extract(url, table_attribs):
     soup = BeautifulSoup(page, "html.parser")
     # Create empty pandas DataFrame with columns as table attributes
     df = pd.DataFrame(columns=table_attribs)
+    #Extract all the attributes of html object
     tables = soup.find_all('tbody')
     rows = tables[2].find_all('tr')
     for row in rows:
         col = row.find_all('td')
-        if len(col) !=0:
+        if len(col) != 0:
             if col[0].find('a') is not None and '—' not in col[2]:
-                data_dict = {"Country": col[0].a.contents[0],
-                             "GDP_USD_millions": col[2].contents[0]}
+                data_dict = {table_attribs[0]:col[0].a.contents[0],
+                             table_attribs[1]: col[2].contents[0]}
                 df1 = pd.DataFrame(data_dict, index=[0])
-                df = pd.concat([df,df1], ignore_index=True)
+                df = pd.concat([df, df1], ignore_index=True)
     return df
+print(extract(url, table_attributes))
